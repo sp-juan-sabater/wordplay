677ea0d0d8b5f75fca41a02f7e35fe6992d2cf5d
JUAN-SABATER\juan-sabater <juan.sabater@socialpoint.es>
2019-08-26 15:49:25 +0200

    updates

---
+++ Wordplay/Assets/Scripts/ToTrash.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ToTrash : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+
+        Destroy(eventData.pointerDrag);
+    }
+}
